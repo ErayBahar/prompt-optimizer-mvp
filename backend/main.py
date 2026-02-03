2a1a702e7a4c55448dbf1018061946ca8495e36e
Eray <eraybahar24@gmail.com>
2026-02-03 16:42:55 +0300

    updated link

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:5175",
         "http://127.0.0.1:5176",
         "https://prompt-optimizer-mvp-tau.vercel.app",
-        "https://prompt-optimizer-mvp-3.onrender.com",
+        "https://prompt-optimizer-mvp-7.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
